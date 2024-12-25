19a76836e01c5ddd903b97c6c9292a9ea40b7bbc
bong-coding <qhdals1611@naver.com>
2024-12-25 22:19:37 +0900

    main

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import "vue-toastification/dist/index.css";
 
 const app = createApp(App);
 
-// 카카오 SDK 초기화 왜 배포가 앚ㄴ되는거지
+// 카카오 SDK 초기화 왜 배포가 앚ㄴ되는거지 왜 안되냐고
 if (window.Kakao && !window.Kakao.isInitialized()) {
   window.Kakao.init(process.env.VUE_APP_KAKAO_JAVASCRIPT_KEY);
   console.log("Kakao SDK Initialized in main.js!");
